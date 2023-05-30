c811d2333073d0d3fbd78f82af4bb50bed4501c7
Aashish Tangnami <42236161+AashishTangnami@users.noreply.github.com>
2023-05-30 14:19:13 -0400

    changed about me

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -11,7 +11,7 @@ const Main = () => {
             <div className='max-w-[1240px] w-full h-full mx-auto p-2 flex justify-center items-center'>
                 <div>
                     <p className='uppercase text-sm tracking-widest text-gray-600'>
-                        Hi! Welcome  to my Digital Portfolio
+                        Hi! Welcome  to my Portfolio
                     </p>
                     <h1> I'm <span className='text-[#252653]'> Aashish Tangnami </span></h1>
                     <h3>
@@ -19,12 +19,11 @@ const Main = () => {
                     </h3>
                     <h5> Soon to be :) </h5>
                     <p className='py-2 max-w-[70%] m-auto'>
-                    I am currently a full-time AI and Data Science Graduate Student in Loyalist College.
-                    I have a passion for technology and have been developing my skills in Artificial Intelligence and Data Science. 
-                    With an interest in AI, I started learning and building my fundamentals in Python, Numpy, Pandas and other technologies. 
-                    I am still sharpening my skills through many courses, outside of my academic syllabus which I would be completing along side my Graduate Degree.
+                    I am currently a full-time AI and Data Science Graduate Student. I have undergrad Software engineering Degree.
+                    I have about 2 years of professional work experience as Software Engineering with different technical stacks such as Python, JavaScript and Elasticsearch.
+                    
                     </p>
-                    <div className='flex items-center justify-between max-w-[330px] m-auto py-4'>
+                    <div className='flex items-center justify-between max-w-[330px] m-auto py-4 mt-12'>
                         <div className='rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-112 ease-in duration-300'>
                             <Link href='https://www.linkedin.com/in/aashishtangnami/'>
                                 <AiFillLinkedin/>
